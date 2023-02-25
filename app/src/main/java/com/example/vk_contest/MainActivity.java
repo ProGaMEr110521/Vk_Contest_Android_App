e3e0fdc6b412541eefc4ed7f32b758015280c8d5
ProGaMEr110521 <vladtem3943@gmail.com>
2023-02-25 10:47:07 +0300

    upd 4

--- app/src/main/java/com/example/vk_contest/MainActivity.java
+++ app/src/main/java/com/example/vk_contest/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -32,11 +33,9 @@ public class MainActivity extends AppCompatActivity{
                 });
         AlertDialog alert = builder.create();
         alert.show();
+
+//        public void OpenContacts() {
+//        Intent intent = new Intent(Intent.ACTION_VIEW);
+//        }
     }
-////    public void onClick(View view){
-////        Intent intent;
-////        switch (view.getId()) {
-////            case R.id.
-////        }
-//    }
 }
---
+++ app/src/main/java/com/example/vk_contest/OpenContacts.java
@@ -0,0 +1,8 @@
+package com.example.vk_contest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.content.Intent;
+
+
