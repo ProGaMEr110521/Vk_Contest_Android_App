9db59704706dc60d7527932b2e9afddb64f98258
ProGaMEr110521 <vladtem3943@gmail.com>
2023-02-28 23:15:38 +0300

    RELEASE
    Я закончил почти все кроме смены участников местами и жаль, что я не смог с этим разобраться. Даже если я не пройду дальше по этапу это был невероятный реальный опыт дедлайнов и работы с андроидом. Надеюсь на лучшее

--- app/src/main/java/com/example/vk_contest/MainActivity.java
+++ app/src/main/java/com/example/vk_contest/MainActivity.java
@@ -1,19 +1,20 @@
 package com.example.vk_contest;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
-import android.app.IntentService;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity{
+    public static boolean flag = true;
+    public static boolean flag2 = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,13 +23,31 @@ public class MainActivity extends AppCompatActivity{
 
     public void showDialogHand(View view) {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("Приветик :3")
-                .setMessage("Привет сладенький хихихи")
+        builder.setTitle("Помаши ручкой")
+                .setMessage("Привет дружище!")
                 .setCancelable(true)
-                .setNegativeButton("Оке", new DialogInterface.OnClickListener() {
+                .setNegativeButton("Привет", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getApplicationContext(), "Ну давай)", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Приятно иметь дело с тобой", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void ShowInProgress(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Вот незадача!")
+                .setMessage("Извините эта функция была не доработана автором, по причине автор" +
+                        " был в отчаянии от взлетов, падений и неудач в процессе своей своей работы." +
+                        "\n Идентификационный номер ошибки:\n java.android.ErrorType:ЁмаёАйНидХелпХир\n \n" +
+                        "Автор 22:27 28.02.23")
+                .setCancelable(true)
+                .setNegativeButton("Хорошо", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Спасибо за понимание)", Toast.LENGTH_SHORT).show();
                     }
                 });
         AlertDialog alert = builder.create();
@@ -45,7 +64,43 @@ public class MainActivity extends AppCompatActivity{
         intent.setType("vnd.android-dir/mms-sms");
         startActivity(intent);
     }
+
     public void onCallButtonClick(View view) {
         android.os.Process.killProcess(android.os.Process.myPid());
     }
+    public void ImageButtonChange1(View view){
+        FloatingActionButton imageButton = (FloatingActionButton) findViewById(R.id.floatingActionButton2);;
+        ImageView imageView = (ImageView) findViewById(R.id.imageView3);
+        imageButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                if (flag) {
+                    imageView.setImageResource(R.drawable.microphone_off);
+                    imageButton.setImageResource(R.drawable.microphone_off);
+                    flag = false;
+                } else {
+                    imageView.setImageResource(R.drawable.microphone_active);
+                    imageButton.setImageResource(R.drawable.microphone_active);
+                    flag = true;
+                }
+            }
+        });
+    }
+    public void ImageButtonChange2(View view) {
+        FloatingActionButton imageButton = (FloatingActionButton) findViewById(R.id.floatingActionButton5);
+        ;
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (flag2) {
+                    imageButton.setImageResource(R.drawable.videocam_off);
+                    flag2 = false;
+                } else {
+                    imageButton.setImageResource(R.drawable.videocam_active);
+                    flag2 = true;
+                }
+            }
+        });
+    }
+
 }
